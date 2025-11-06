11db4f270bf76ab26409262ae32413c19d5c1e8b
Sujith <sujithsanthosh0304@gmail.com>
2025-11-06 22:35:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (39.84%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# first submission
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
