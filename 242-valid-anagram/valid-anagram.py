17101df750addca64e683c3245cd7d9f23ab3b77
Sujith <sujithsanthosh0304@gmail.com>
2026-01-29 10:54:42 +0000

    Time: 11 ms (75.42%) | Memory: 19.4 MB (31.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,9 +3,8 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        countS, countT = {}, {}
+        s_map, t_map = {}, {}
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-            countT[t[i]] = 1 + countT.get(t[i],0)
-        return countS == countT
-        
+            s_map[s[i]] = 1 + s_map.get(s[i], 0)
+            t_map[t[i]] = 1 + t_map.get(t[i], 0)
+        return s_map == t_map
