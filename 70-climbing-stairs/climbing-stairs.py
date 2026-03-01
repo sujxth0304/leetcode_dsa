aab430b453441462c169142adc8754935c2c700f
Sujith <sujithsanthosh0304@gmail.com>
2026-03-01 22:29:39 +0530

    Time: 1 ms (3.65%) | Memory: 19.2 MB (78.48%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,23 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # top down memoization
-        # memo = {1:1, 2:2}
-        # def f(n):
-        #     if n in memo:
-        #         return memo[n]
-        #     else:
-        #         memo[n] = f(n-1) + f(n-2)
-        #         return memo[n]
-        # return f(n)
+        memo = {1:1, 2:2}
+        def f(n):
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = f(n-2) + f(n-1)
+                return memo[n]
 
-        # bottom up tabulation
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        dp = [0]*n
-        dp[0] = 1
-        dp[1] = 2
-        for i in range(2, n):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n-1]
+
+        return f(n)
